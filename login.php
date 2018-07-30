f1fd2b09eae21748832583f5c0456dab8c326a2c
Nico <thouvenin.nicolas@live.fr>
2018-07-30 14:04:49 +0200

    retest

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Manga Reader Online</title>
+    </head>
+    <body>
+        <div id="page">
+            <div id="logo">
+                <h1>LOGO</h1>
+            </div> <!-- logo -->
+            <div id="bouton">
+                <input type="button" value="login">
+                <input type="button" value="Sign in">
+            </div> <!-- bouttons -->
+        </div> <!-- page -->
+    </body>
+</html>
