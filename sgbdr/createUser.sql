c5ce9e3e770a58ed4ef90b86402cbd4a3a695531
hlebon <henri.lebon@res-group.com>
2018-08-01 09:24:55 +0200

    script procédure stockée création user correction faute dans commentaire

--- sgbdr/createUser.sql
+++ sgbdr/createUser.sql
@@ -1,6 +1,6 @@
 DELIMITER $$
 CREATE PROCEDURE createUser(IN inLogin VARCHAR(255), IN inFirstname VARCHAR(255), IN inSurname VARCHAR(255), IN inBirthDate VARCHAR(10), IN inPassword VARCHAR(255), IN inEmail VARCHAR(254))
-/* Cette procédure créer un nouvelle utilisateur dans la base. Celui si est non validé par défaut. */
+/* Cette procédure créer un nouvelle utilisateur dans la base. Celui-ci est non validé par défaut. */
 BEGIN
 	INSERT INTO users (Login, Firstname, Surname, BirthDate, Password, Email, EmailValided) VALUES (inLogin, inFirstname, inSurname, inBirthDate, PASSWORD(inPassword), inEmail, 0);
 END $$
