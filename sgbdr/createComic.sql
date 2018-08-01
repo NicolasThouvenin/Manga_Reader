544f5b6c4db4f7837f5ccc7490b6930867b40005
hlebon <henri.lebon@res-group.com>
2018-08-01 10:06:57 +0200

    procédure stockée createUser ajout du retour de l'Id et de la création d'une ligne dans la table Authors

--- sgbdr/createComic.sql
+++ sgbdr/createComic.sql
@@ -1,6 +1,9 @@
 DELIMITER $$
-CREATE PROCEDURE createComic(IN inTitle VARCHAR(255), IN inSynopsie VARCHAR(255), IN inStartDate VARCHAR(10))
-/* Cette procédure créer un nouveau comic dans la base. Celui-ci est non validé par défaut. */
+CREATE PROCEDURE createComic(IN inTitle VARCHAR(255), IN inSynopsie VARCHAR(255), IN inStartDate VARCHAR(10), IN inAuthorId INTEGER, OUT lastComicId INTEGER)
+/* Cette procédure créer un nouveau comic dans la base et retourne son Id. La table Authors est également mise à jour. */
 BEGIN
+	SET lastComicId = -1;
 	INSERT INTO comics (Title, Synopsie, StartDate) VALUES (inTitle, inSynopsie, inStartDate);
+	SET lastComicId = LAST_INSERT_ID();
+	INSERT INTO Authors (comicId, userId) VALUES (lastComicId, inAuthorId);
 END $$
