223e150246428451cd9ba10da8f03f15569d04e7
hlebon <henri.lebon@res-group.com>
2018-08-02 15:09:16 +0200

    procédure stockée getComicsWithValidatedChapter correction du nom

--- sgbdr/getComicsWithValidatedChapter.sql
+++ sgbdr/getComicsWithValidatedChapter.sql
@@ -1,29 +1,26 @@
 DELIMITER $$
-CREATE PROCEDURE getComicsWithValidedChapter()
+CREATE PROCEDURE getComicsWithValidatedChapter()
 /* Cette procédure stockée renvoie la liste des comics avec au moins un chapitre validé */
 BEGIN
-
 	SELECT comics.*
 	FROM comics
 	WHERE comics.Id IN
 
 	(
-		SELECT DISTINCT narrativeArcs.comicId
+		SELECT narrativeArcs.comicId
 		FROM narrativeArcs
 		WHERE narrativeArcs.Id IN
 
 		(
-			SELECT DISTINCT volumes.narrativeArcId
+			SELECT volumes.narrativeArcId
 			FROM volumes
 			WHERE volumes.Id IN
 
 			(
-				SELECT DISTINCT chapters.volumeId
+				SELECT chapters.volumeId
 				FROM chapters
 				WHERE chapters.Validated = 1
 			)
 		)
-
 	);
-
 END $$
