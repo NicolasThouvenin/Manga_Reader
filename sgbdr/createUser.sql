872093aed7fcf31fb39752bf1e543723cd2ded86
hlebon <henri.lebon@res-group.com>
2018-08-02 15:22:36 +0200

    formulaire et ajout de nouvel utilisateur sécurisé

--- sgbdr/createUser.sql
+++ sgbdr/createUser.sql
@@ -1,6 +1,8 @@
 DELIMITER $$
-CREATE PROCEDURE createUser(IN inLogin VARCHAR(255), IN inFirstname VARCHAR(255), IN inSurname VARCHAR(255), IN inBirthDate VARCHAR(10), IN inPassword VARCHAR(255), IN inEmail VARCHAR(254))
+CREATE PROCEDURE createUser(IN inLogin VARCHAR(255), IN inFirstname VARCHAR(255), IN inSurname VARCHAR(255), IN inBirthDate VARCHAR(10), IN inPassword VARCHAR(255), IN inEmail VARCHAR(254), OUT lastUserId INTEGER)
 /* Cette procédure créer un nouvelle utilisateur dans la base. Celui-ci est non validé par défaut. */
 BEGIN
+	SET lastUserId = -1;
 	INSERT INTO users (Login, Firstname, Surname, BirthDate, Password, Email, EmailValided) VALUES (inLogin, inFirstname, inSurname, inBirthDate, PASSWORD(inPassword), inEmail, 0);
+	SET lastUserId = LAST_INSERT_ID();
 END $$
