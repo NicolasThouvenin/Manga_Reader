97c0efae2905b466539b23ef0c9d6e13b0a4e6c8
hlebon <henri.lebon@res-group.com>
2018-08-02 14:02:27 +0200

    procédure stockée renvoyant les informations d'une BD avec les informations connexes

--- sgbdr/getComicData.sql
+++ sgbdr/getComicData.sql
@@ -9,7 +9,7 @@ CREATE PROCEDURE getComicData(IN inComicId INTEGER)
 	- Arborescence Arc, Tome, Chapitre avec leurs dates
  */
 BEGIN
-	SELECT comics.Id, comics.Title, comics.Synopsie, comics.StartDate, comics.EndDate, comics.CoverExt, filteredUsers.Login, filteredUsers.Firstname, filteredUsers.Surname, GROUP_CONCAT(searchComics.genreId), narrativeArcsData.data
+	SELECT comics.Id, comics.Title, comics.Synopsie, comics.StartDate, comics.EndDate, comics.CoverExt, filteredUsers.Login, filteredUsers.Firstname, filteredUsers.Surname, GROUP_CONCAT(searchComics.genreId) as Genres, narrativeArcsData.data as arcs
 	FROM comics
 	JOIN (
 	    	SELECT users.Login, users.Firstname, users.Surname, authors.comicId 
@@ -22,20 +22,19 @@ BEGIN
 	searchComics
 	ON comics.Id = searchComics.comicId
 	JOIN (
-			SELECT n.comicId, CONCAT('{', 'number:', n.Number, ',title:"', n.Title, '",synopsie:"', n.Synopsie, '",startDate:"', CAST(n.StartDate AS CHAR), '",endDate:"', IF(n.EndDate, CAST(n.EndDate AS CHAR), ''), '",volumes:', volumesData.data, '}') as data
+			SELECT n.comicId, CONCAT('{', '"number":', n.Number, ',"title":"', n.Title, '","synopsie":"', n.Synopsie, '","startDate":"', CAST(n.StartDate AS CHAR), '","endDate":"', IF(n.EndDate, CAST(n.EndDate AS CHAR), ''), '","volumes":', volumesData.data, '}') as data
 			FROM narrativeArcs AS n
 			JOIN (
-				SELECT v.narrativeArcId, CONCAT('{', 'number:', v.Number, ',title:"', v.Title, '",synopsie:"', v.Synopsie, '",startDate:"', CAST(v.StartDate AS CHAR), '",endDate:"', IF(v.EndDate, CAST(v.EndDate AS CHAR), ''), '"}') as data
+				SELECT v.narrativeArcId, CONCAT('{', '"number":', v.Number, ',"title":"', v.Title, '","synopsie":"', v.Synopsie, '","startDate":"', CAST(v.StartDate AS CHAR), '","endDate":"', IF(v.EndDate, CAST(v.EndDate AS CHAR), ''), '","chapters":', chaptersData.data, '}') as data
 				FROM volumes AS v
+				JOIN (
+					SELECT c.volumeId, CONCAT('{', '"number":', c.Number, ',"title":"', c.Title, '","synopsie":"', c.Synopsie, '","validated":', .c.Validated, ',"publicationDate":"', IF(c.PublicationDate, CAST(c.PublicationDate AS CHAR), ''), '"}') as data
+					FROM chapters AS c
+					) as chaptersData
+				ON chaptersData.volumeId = v.Id
 				) AS volumesData
 			ON volumesData.narrativeArcId = n.Id
 		) AS narrativeArcsData
 	ON narrativeArcsData.comicId = comics.Id
 	GROUP BY comics.Id;
 END $$
-
-
-CAST(v.EndDate AS CHAR)
-
-
-IF(v.EndDate, CAST(v.EndDate AS CHAR), '')
