10d3e8572f0dbad7eb963807b481adf94106170d
hlebon <henri.lebon@res-group.com>
2018-08-04 11:21:47 +0200

    suppression de la notion d'arc de la base de données

--- sgbdr/deleteArc.sql
+++
@@ -1,6 +0,0 @@
-DELIMITER $$
-CREATE PROCEDURE deleteArc(IN inArcId INTEGER)
-/* Cette procédure supprime un nouvel Arc de la base grâce à son Id. */
-BEGIN
-	DELETE FROM narrativeArcs WHERE Id = inArcId;
-END $$
