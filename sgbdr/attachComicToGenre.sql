d61d2da2561dc3ffc885b875dd3d2184fac6ae7c
hlebon <henri.lebon@res-group.com>
2018-08-04 11:28:21 +0200

    procédure stockée atache comic to genre adaptation au changement de nom de la table

--- sgbdr/attachComicToGenre.sql
+++ sgbdr/attachComicToGenre.sql
@@ -2,5 +2,5 @@ DELIMITER $$
 CREATE PROCEDURE attachComicToGenre(IN inGenreId INTEGER, IN inComicId INTEGER)
 /* Rattache une BD à un genre. */
 BEGIN
-	INSERT INTO searchComics (genreId, comicId) VALUES (inGenreId, inComicId);
+	INSERT INTO comicsgenres (genreId, comicId) VALUES (inGenreId, inComicId);
 END $$
