e753173836eaf8ee6f2084ccad514625024b6ac6
hlebon <henri.lebon@res-group.com>
2018-08-04 11:57:54 +0200

    procédure stockées de suppression de tome et de chapitre ajout d'un décrément des numéros suivant

--- sgbdr/deleteChapter.sql
+++ sgbdr/deleteChapter.sql
@@ -1,6 +1,9 @@
 DELIMITER $$
 CREATE PROCEDURE deleteChapter(IN inChapterId INTEGER)
-/* Cette procédure supprime un chapitre de la base grâce à son Id. */
+/* Cette procédure supprime un chapitre de la base grâce à son Id. Les numéros situé après sont décrémententés. */
 BEGIN
+	DECLARE oldNumber INT;
+	SELECT Number INTO oldNumber FROM chapters;
 	DELETE FROM chapters WHERE Id = inChapterId;
+	UPDATE chapters SET Numero = Numero - 1 WHERE Id = inChapterId AND chapters.Numero > oldNumber;
 END $$
--- sgbdr/deleteVolume.sql
+++ sgbdr/deleteVolume.sql
@@ -1,6 +1,9 @@
 DELIMITER $$
 CREATE PROCEDURE deleteVolume(IN inVolumeId INTEGER)
-/* Cette procédure supprime un tome de la base grâce à son Id. */
+/* Cette procédure supprime un tome de la base grâce à son Id. Les numéros situé après sont décrémententés. */
 BEGIN
+	DECLARE oldNumber INT;
+	SELECT Number INTO oldNumber FROM volumes;
 	DELETE FROM volumes WHERE Id = inVolumeId;
+	UPDATE volumes SET Numero = Numero - 1 WHERE Id = inVolumeId AND volumes.Numero > oldNumber;
 END $$
