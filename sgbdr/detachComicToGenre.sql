a138472b6bfa556f9d2e19ee5f292847a50a191e
hlebon <henri.lebon@res-group.com>
2018-08-04 11:35:14 +0200

    detachComicToGenre renomage de la table de liaison

--- sgbdr/detachComicToGenre.sql
+++ sgbdr/detachComicToGenre.sql
@@ -2,5 +2,5 @@ DELIMITER $$
 CREATE PROCEDURE detachComicFromGenre(IN inGenreId INTEGER, IN inComicId INTEGER)
 /* Déatache une BD à un genre. */
 BEGIN
-	DELETE FROM searchComics WHERE genreId = inGenreId AND comicId = inComicId;
+	DELETE FROM comicsgenres WHERE genreId = inGenreId AND comicId = inComicId;
 END $$
