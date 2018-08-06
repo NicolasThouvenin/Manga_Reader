696a862b28678da5620af61668295f49629bbe08
hlebon <henri.lebon@res-group.com>
2018-08-06 10:33:11 +0200

    procedure getComicsWithValidatedChapter : remplacement de la requête par imbrication

--- sgbdr/getComicsWithValidatedChapter.sql
+++ sgbdr/getComicsWithValidatedChapter.sql
@@ -3,18 +3,10 @@ CREATE PROCEDURE getComicsWithValidatedChapter()
 /* Cette procédure stockée renvoie la liste des comics avec au moins un chapitre validé */
 BEGIN
 	SELECT comics.*
-	FROM comics
-	WHERE comics.Id IN
-
-	(
-		SELECT volumes.comicId
-		FROM volumes
-		WHERE volumes.Id IN
-
-		(
-			SELECT chapters.volumeId
-			FROM chapters
-			WHERE chapters.Validated = 1
-		)
-	);
+	FROM chapters
+	JOIN volumes
+	ON chapters.volumeId = volumes.Id
+	JOIN comics
+	ON volumes.comicId = comics.Id
+	WHERE chapters.Validated = 1;
 END $$
