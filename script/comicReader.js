d572d74d8f1e44eef55a0afaf0ad17f18ca2454e
Nico <thouvenin.nicolas@live.fr>
2018-08-06 17:40:07 +0200

    comicReader.js added

---
+++ script/comicReader.js
@@ -0,0 +1,27 @@
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var bubbles = document.getElementsByClassName("bubble");
+
+    if (n > bubbles.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = bubbles.length;
+    }
+    for (i = 0; i < bubbles.length; i++) {
+        bubbles[i].style.display = "none";
+    }
+
+    bubbles[slideIndex - 1].style.display = "block";
+
+}
