9891c6fbc77f6acd16402cf05ff7568fbd6aa449
Nico <thouvenin.nicolas@live.fr>
2018-08-07 23:41:46 +0200

    creation volume

--- comic.class.php
+++ comic.class.php
@@ -1,253 +1,267 @@
 <?php
 
-    class Comic {
-        
-        protected $Id;
-        private $Title;
-        private $Synopsis;
-        private $StartDate;
-        private $EndDate;
-        private $CoverExt;
-        private $volumesLoaded = false;
-        private $Volumes = array();
-        private $GenreIdsLoaded = false;
-        private $GenreIds = array();
-        private $AuthorsLoaded = false;
-        private $Authors = array();
-        private $ImagesFolder;
+class Comic {
 
-        public function __construct(int $Id) {
+    protected $Id;
+    private $Title;
+    private $Synopsis;
+    private $StartDate;
+    private $EndDate;
+    private $CoverExt;
+    private $volumesLoaded = false;
+    private $Volumes = array();
+    private $GenreIdsLoaded = false;
+    private $GenreIds = array();
+    private $AuthorsLoaded = false;
+    private $Authors = array();
+    private $ImagesFolder;
 
-            $this->Id = $Id;
+    public function __construct(int $Id) {
 
-            try {
+        $this->Id = $Id;
 
-                require('connection.php');
+        try {
 
-                $result = $db->prepare("SELECT * FROM comics WHERE id = :comicId");
-                $result->execute(array('comicId' => 1));
+            require('connection.php');
 
-                while ($line = $result->fetch()) {
+            $result = $db->prepare("SELECT * FROM comics WHERE id = :comicId");
+            $result->execute(array('comicId' => 1));
 
-                    $this->Title = $line['Title'];
-                    $this->Synopsis = $line['Synopsis'];
-                    $this->StartDate = $line['StartDate'];
-                    $this->CoverExt = $line['CoverExt'];
+            while ($line = $result->fetch()) {
 
-                    if ($line['EndDate'] === null) {
-                        $this->EndDate = '';
-                    } else {
-                        $this->EndDate = $line['EndDate'];
-                    }
+                $this->Title = $line['Title'];
+                $this->Synopsis = $line['Synopsis'];
+                $this->StartDate = $line['StartDate'];
+                $this->CoverExt = $line['CoverExt'];
+
+                if ($line['EndDate'] === null) {
+                    $this->EndDate = '';
+                } else {
+                    $this->EndDate = $line['EndDate'];
+                }
+            }
+
+            $this->SetGenreIds();
+            $this->SetAuthors();
+            $this->SetImagesFolder();
+        } catch (Exception $e) {
+            throw new Exception("\nErreur lors de la création de l'objet comic : " . $e->getMessage());
+        }
+    }
+
+    function getId() {
+        return $this->Id;
+    }
+
+    function getTitle() {
+        return $this->Title;
+    }
+
+    function getSynopsis() {
+        return $this->Synopsis;
+    }
+
+    function getStartDate() {
+        return $this->StartDate;
+    }
+
+    function getEndDate() {
+        return $this->EndDate;
+    }
+
+    function getCoverExt() {
+        return $this->CoverExt;
+    }
+
+    function setTitle($Title) {
+        $this->Title = $Title;
+    }
+
+    function setSynopsis($Synopsis) {
+        $this->Synopsis = $Synopsis;
+    }
+
+    function setStartDate($StartDate) {
+        $this->StartDate = $StartDate;
+    }
+
+    function setEndDate($EndDate) {
+        $this->EndDate = $EndDate;
+    }
+
+    private function setCoverExt($CoverExt) {
+
+        try {
+
+            require('connection.php');
+
+            $result = $db->prepare("UPDATE comics SET CoverExt = :CoverExt WHERE Id = :Id");
+            $result->execute(array('Id' => $this->Id, 'CoverExt' => $CoverExt));
+
+            $this->CoverExt = $CoverExt;
+        } catch (Exception $e) {
+            throw new Exception("\nErreur lors l'insertion de la nouvelle extension du cover dans la base de données : " . $e->getMessage());
+        }
+    }
+
+    private function SetVolumes() {
+
+        try {
+
+            require('connection.php');
+
+            $result = $db->prepare("SELECT * FROM volumes WHERE comicId = :comicId");
+            $result->execute(array('comicId' => $this->Id));
+
+            while ($line = $result->fetch()) {
+
+                if ($line['EndDate'] === null) {
+                    $endDate = '';
+                } else {
+                    $endDate = $line['EndDate'];
                 }
 
-                $this->SetGenreIds();
-                $this->SetAuthors();
-                $this->SetImagesFolder();
+                $volume = new Volume($line['Id'], $line['Number'], $line['Title'], $line['Synopsis'], $line['StartDate'], $endDate);
 
-            }  catch (Exception $e) {
-                throw new Exception("\nErreur lors de la création de l'objet comic : ".$e->getMessage());
+                $this->Volumes[$line['Id']] = $volume;
             }
+        } catch (Exception $e) {
+            throw new Exception("\nErreur lors de la création de la liste de volumes : " . $e->getMessage());
         }
-        
-        function getId() {
-            return $this->Id;
-        }
+    }
 
-        function getTitle() {
-            return $this->Title;
-        }
-
-        function getSynopsis() {
-            return $this->Synopsis;
+    public function getVolumes() {
+        if (!$this->volumesLoaded) {
+            $this->SetVolumes();
+            $this->volumesLoaded = true;
         }
 
-        function getStartDate() {
-            return $this->StartDate;
+        foreach ($this->Volumes as $volume) {
+            yield $volume;
         }
+    }
 
-        function getEndDate() {
-            return $this->EndDate;
-        }
+    private function SetGenreIds() {
 
-        function getCoverExt() {
-            return $this->CoverExt;
-        }
+        try {
 
-        function setTitle($Title) {
-            $this->Title = $Title;
-        }
+            require('connection.php');
 
-        function setSynopsis($Synopsis) {
-            $this->Synopsis = $Synopsis;
-        }
+            $result = $db->prepare("SELECT genreId FROM comicsgenres WHERE comicId = :comicId");
+            $result->execute(array('comicId' => $this->Id));
 
-        function setStartDate($StartDate) {
-            $this->StartDate = $StartDate;
-        }
-
-        function setEndDate($EndDate) {
-            $this->EndDate = $EndDate;
-        }
-
-        private function setCoverExt($CoverExt) {
-
-            try {
-
-                require('connection.php');
-                
-                $result = $db->prepare("UPDATE comics SET CoverExt = :CoverExt WHERE Id = :Id");
-                $result->execute(array('Id' => $this->Id, 'CoverExt' => $CoverExt));
-
-                $this->CoverExt = $CoverExt;
-
-            } catch (Exception $e) {
-                throw new Exception("\nErreur lors l'insertion de la nouvelle extension du cover dans la base de données : ".$e->getMessage());
+            while ($line = $result->fetch()) {
+                array_push($this->GenreIds, $line['genreId']);
             }
-            
+        } catch (Exception $e) {
+            throw new Exception("\nErreur lors de la création de la liste de d'indentifiant de genres : " . $e->getMessage());
         }
+    }
 
-        private function SetVolumes() {
-
-            try {
-
-                require('connection.php');
-                
-                $result = $db->prepare("SELECT * FROM volumes WHERE comicId = :comicId");
-                $result->execute(array('comicId' => $this->Id));
-
-                while ($line = $result->fetch()) {
-
-                    if ($line['EndDate'] === null) {
-                        $endDate = '';
-                    } else {
-                        $endDate = $line['EndDate'];
-                    }
-
-                    $volume = new Volume($line['Id'], $line['Number'], $line['Title'], $line['Synopsis'], $line['StartDate'], $endDate);
-
-                    $this->Volumes[$line['Id']] = $volume;
-                }
-
-            }  catch (Exception $e) {
-                throw new Exception("\nErreur lors de la création de la liste de volumes : ".$e->getMessage());
-            }
+    function getGenreIds() {
+        if (!$this->GenreIdsLoaded) {
+            $this->SetGenreIds();
+            $this->GenreIdsLoaded = true;
         }
-
-        public function getVolumes() {
-            if (!$this->volumesLoaded) {
-               $this->SetVolumes();
-               $this->volumesLoaded = true;
-            }
-            
-            foreach ($this->Volumes as $volume) {
-                yield $volume;
-            }
+        foreach ($this->GenreIds as $genreId) {
+            yield $genreId;
         }
+    }
 
-        private function SetGenreIds() {
+    private function SetAuthors() {
 
-            try {
+        try {
 
-                require('connection.php');
-                
-                $result = $db->prepare("SELECT genreId FROM comicsgenres WHERE comicId = :comicId");
-                $result->execute(array('comicId' => $this->Id));
+            require('connection.php');
 
-                while ($line = $result->fetch()) {
-                    array_push($this->GenreIds, $line['genreId']);
-                }
-
-            } catch (Exception $e) {
-                throw new Exception("\nErreur lors de la création de la liste de d'indentifiant de genres : ".$e->getMessage());
-            }
-
-        }
-
-        function getGenreIds() {
-            if (!$this->GenreIdsLoaded) {
-                $this->SetGenreIds();
-                $this->GenreIdsLoaded = true;
-            }
-        	foreach ($this->GenreIds as $genreId) {
-                yield $genreId;
-            }
-        }
-
-        private function SetAuthors() {
-
-            try {
-
-                require('connection.php');
-
-                $query = "SELECT users.* FROM users 
+            $query = "SELECT users.* FROM users 
                 JOIN authors
                 ON users.Id = authors.userId
                 WHERE authors.comicId = :comicId";
-                
-                $result = $db->prepare($query);
-                $result->execute(array('comicId' => $this->Id));
 
-                while ($line = $result->fetch()) {
-                    $author = new Author($line['Id']);
-                    $this->Authors[$line['Id']] = $author;
-                }
-            } catch (Exception $e) {
-                throw new Exception("\nErreur lors de la création de la liste d'objets auteurs : ".$e->getMessage());
+            $result = $db->prepare($query);
+            $result->execute(array('comicId' => $this->Id));
+
+            while ($line = $result->fetch()) {
+                $author = new Author($line['Id']);
+                $this->Authors[$line['Id']] = $author;
             }
-
+        } catch (Exception $e) {
+            throw new Exception("\nErreur lors de la création de la liste d'objets auteurs : " . $e->getMessage());
         }
-
-        function getAuthors() {
-            if (!$this->AuthorsLoaded) {
-                $this->SetAuthors();
-                $this->AuthorsLoaded = true;
-            }
-        	foreach ($this->Authors as $author) {
-                yield $author;
-            }
-        }
-
-        function AddCover($coverTmpName) {
-            try {
-
-                $fileExt = $this->GetExtension($coverTmpName);
-
-                $this->setCoverExt($fileExt);
-
-                $FilePath = $this->ImagesFolder.'\\cover.'.$fileExt;
-                if(!move_uploaded_file($coverTmpName, $FilePath)) {
-                    throw new Exception("\nL'image du cover n'a pas pu être ajouté par move_uploaded_file : ".$e->getMessage());
-                }
-            } catch (Exception $e) {
-                throw new Exception("\nErreur lors de l'ajout du cover : ".$e->getMessage());
-            }
-
-        }
-
-        function getCover() {
-            return $this->ImagesFolder.'\\cover.'.$this->CoverExt;
-        }
-
-        private function SetImagesFolder() {
-            try {
-                $this->ImagesFolder = 'comics\\'.$this->Id;
-                if (!file_exists($this->ImagesFolder)) {
-                    mkdir($this->ImagesFolder, 0777, true);
-                }
-            } catch (Exception $e) {
-                throw new Exception("\nErreur lors de la création du dossier d'image du comic : ".$e->getMessage());
-            }
-        }
-
-        private function GetExtension($tmpName) {
-            $check = explode('/', mime_content_type($tmpName));
-            if ($check[0] !== 'image') {
-                throw new Exception("\nLe fichier n'est pas une image : ".$e->getMessage());
-            } else {
-                return $check[1];
-            }
-        }  
-
     }
+
+    function getAuthors() {
+        if (!$this->AuthorsLoaded) {
+            $this->SetAuthors();
+            $this->AuthorsLoaded = true;
+        }
+        foreach ($this->Authors as $author) {
+            yield $author;
+        }
+    }
+
+    function AddCover($coverTmpName) {
+        try {
+
+            $fileExt = $this->GetExtension($coverTmpName);
+
+            $this->setCoverExt($fileExt);
+
+            $FilePath = $this->ImagesFolder . '\\cover.' . $fileExt;
+            if (!move_uploaded_file($coverTmpName, $FilePath)) {
+                throw new Exception("\nL'image du cover n'a pas pu être ajouté par move_uploaded_file : " . $e->getMessage());
+            }
+        } catch (Exception $e) {
+            throw new Exception("\nErreur lors de l'ajout du cover : " . $e->getMessage());
+        }
+    }
+
+    function getCover() {
+        return $this->ImagesFolder . '\\cover.' . $this->CoverExt;
+    }
+
+    private function SetImagesFolder() {
+        try {
+            $this->ImagesFolder = 'comics\\' . $this->Id;
+            if (!file_exists($this->ImagesFolder)) {
+                mkdir($this->ImagesFolder, 0777, true);
+            }
+        } catch (Exception $e) {
+            throw new Exception("\nErreur lors de la création du dossier d'image du comic : " . $e->getMessage());
+        }
+    }
+
+    private function GetExtension($tmpName) {
+        $check = explode('/', mime_content_type($tmpName));
+        if ($check[0] !== 'image') {
+            throw new Exception("\nLe fichier n'est pas une image : " . $e->getMessage());
+        } else {
+            return $check[1];
+        }
+    }
+
+    function createVolume($title, $synopsis) {
+        try {
+            require('connection.php');
+            $date = date("Y-m-d");
+            $addVolume = $db->prepare("CALL createVolume(:title, :synopsis, :date, :comicId, @lastVolumeId");
+            $addVolume->bindParam(':title', $title, PDO::PARAM_STR, 255);
+            $addVolume->bindParam(':synopsis', $synopsis, PDO::PARAM_STR, 255);
+            $addVolume->bindParam(':date', $date, PDO::PARAM_STR, 10);
+            $addVolume->bindParam(':comicId', $this->Id, PDO::PARAM_INT);
+            $addVolume->execute();
+            $addVolume->closeCursor();
+
+            $result = $db->query("SELECT @lastVolumeId")->fetch(PDO::FETCH_ASSOC);
+
+            $volume = new Volume($result['@lastVolumeId'], $Number, $title, $synopsis, $date, "");
+            $this->Volumes[$result['@lastVolumeId']] = $volume;
+        } catch (Exception $e) {
+            throw new Exception("\nError during new volume creation : " . $e->getMessage());
+        }
+    }
+
+}
+
 ?>
--- comic.php
+++ comic.php
@@ -1,3 +1,25 @@
+<?php
+require('required.php');
+
+//If no comic have been passed in GET, return to homepage
+if (!isset($_GET["bookId"])) {
+    header("Location:homePage.php");
+}
+if (isset($_POST["submit"])) {
+    try {
+        $comic = new Comic($_GET["bookId"]);
+        $comic->createVolume(htmlentities($_POST["submit"]), htmlentities($_POST["submit"]));
+        header("Location:newChapter.php");
+    } catch (Exception $e) {
+        die('Error : ' . $e->getMessage());
+    }
+}
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -17,9 +39,6 @@
 
                 <div class="displayUser">
                     <?php
-                    require('required.php');
-
-                    session_start();
                     if (isset($_COOKIE['authentified'])) {
                         $user = unserialize($_COOKIE['authentified']);
                         // if a user is connected
@@ -42,80 +61,89 @@
 
                 <!-- *****************    Page Content ***************** -->
                 <?php
-                if (isset($_GET["bookId"])) {
-                    try {
+                try {
 
-                            $comic = new Comic($_GET["bookId"]);
-                            
-                            echo '<script>';
-                            echo 'var title = "' . $comic->getTitle() . '";';
-                            echo '</script>';
-                            ?>
-                            <div id="book_cover">
-                                <img title="<?php echo $comic->getTitle(); ?>" src="<?php echo "comics\\" . $_GET['bookId'] . "\\cover." . $comic->getCoverExt(); ?>" alt="cover" height="375" width="250">
-                            </div> <!-- book_cover -->
-                            <div id="book_meta">
-                                <p id="book_title"><?php echo $comic->getTitle(); ?></p>
-                                <p id="book_author"><?php
-                                    foreach ($comic->getAuthors() as $author) {
-                                        echo $author->getLogin() . ", ";
-                                    }
-                                    ?></p>
-                                <p id="book_release"><?php echo $comic->getStartDate(); ?></p>
-                                <p id="book_genre"><?php
-                                    foreach ($comic->getGenreIds() as $genreId) {
-                                        echo $genreId . ", ";
-                                    }
-                                    ?>
-                                </p>
-                                <p id="synopsis"><?php echo $comic->getSynopsis(); ?></p>
+                    $comic = new Comic($_GET["bookId"]);
 
-                            </div>  <!-- book_meta -->
-                            <div id="chapters">
-                                <p id="chapters_header"><?php echo $comic->getTitle(); ?> Chapters</p>
-
-                                <!--   Generated Part    -->
-
-                                <?php
-                                foreach ($comic->getVolumes() as $volume) {
-                                    ?>
-                                    <div id='Volume <?php echo $volume->getNumber(); ?> '>
-                                        <p class="volume">Volume <?php echo $volume->getNumber(); ?>
-                                            <span class='volume_title'><?php echo $volume->getTitle(); ?></span>
-                                            <span class="volume_synopsis"><?php echo $volume->getSynopsis(); ?></span>                                    
-                                            <span class="volume_release"><?php echo $volume->getStartDate(); ?></span></p>
-                                        <ul>
-                                            <?php
-                                            foreach ($volume->getChapters() as $chapter) {
-                                                ?>
-                                                <li>
-                                                    <p class='chapter'><a href="comicsReader.php">Chapter <?php echo $chapter->getNumber(); ?>
-                                                            <span class='chapter_title' title="<?php echo $chapter->getSynopsis() ?>"><?php echo $chapter->getTitle() ?></span>
-                                                            <span class="chapter_release"><?php echo $chapter->getPublicationDate() ?></span>
-                                                        </a></p>
-                                                </li>
-                                                <?php
-                                            }
-                                            ?>
-                                        </ul>
-                                    </div> <!-- Volume <?php echo $volume->getNumber(); ?>  "--> 
-                                    <?php
+                    echo '<script>';
+                    echo 'var title = "' . $comic->getTitle() . '";';
+                    echo '</script>';
+                    ?>
+                    <div id="book_cover">
+                        <img title="<?php echo $comic->getTitle(); ?>" src="<?php echo "comics\\" . $_GET['bookId'] . "\\cover." . $comic->getCoverExt(); ?>" alt="cover" height="375" width="250">
+                    </div> <!-- book_cover -->
+                    <div id="book_meta">
+                        <p id="book_title"><?php echo $comic->getTitle(); ?></p>
+                        <p id="book_author"><?php
+                            foreach ($comic->getAuthors() as $author) {
+                                if ($author->getId() == $user->getId()) {
+                                    echo "<b>" . $author->getLogin() . "</b>, ";
+                                    $isAuthor = true;
+                                } else {
+                                    echo $author->getLogin() . ", ";
                                 }
-                                ?>
+                            }
+                            ?></p>
+                        <p id="book_release"><?php echo $comic->getStartDate(); ?></p>
+                        <p id="book_genre"><?php
+                            foreach ($comic->getGenreIds() as $genreId) {
+                                echo $genreId . ", ";
+                            }
+                            ?>
+                        </p>
+                        <p id="synopsis"><?php echo $comic->getSynopsis(); ?></p>
+                        <?php echo date("Y-m-d"); ?>
 
-                            </div> <!-- chapters -->
+                    </div>  <!-- book_meta -->
+                    <div id="add_new">
+                        <span id="add_volume" onclick="toggleForm()">Create New Volume</span>
+                        <span id="add_chapter"><a href="newChapter.php">Create New Chapter</a></span>
+                        <form id="new_volume_form" method="POST" action="comic.php">
+                            <p></p><input type="text" name="title" placeholder="Volume Title" required></p>
+                            <p><textarea name="synopsis" placeholder="Volume Synopsis" cols="40" rows="3" required></textarea></p>
+                            <input type="submit" name="submit" value="Create Volume">
+                        </form>
+                    </div>
+                    <div id="chapters">
+                        <p id="chapters_header"><?php echo $comic->getTitle(); ?> Chapters</p>
+
+                        <!--   Generated Part    -->
+
+                        <?php
+                        foreach ($comic->getVolumes() as $volume) {
+                            ?>
+                            <div id='Volume <?php echo $volume->getNumber(); ?> '>
+                                <p class="volume">Volume <?php echo $volume->getNumber(); ?>
+                                    <span class='volume_title'><?php echo $volume->getTitle(); ?></span>
+                                    <span class="volume_synopsis"><?php echo $volume->getSynopsis(); ?></span>                                    
+                                    <span class="volume_release"><?php echo $volume->getStartDate(); ?></span></p>
+                                <ul>
+                                    <?php
+                                    foreach ($volume->getChapters() as $chapter) {
+                                        ?>
+                                        <li>
+                                            <p class='chapter'><a href="comicsReader.php">Chapter <?php echo $chapter->getNumber(); ?>
+                                                    <span class='chapter_title' title="<?php echo $chapter->getSynopsis() ?>"><?php echo $chapter->getTitle() ?></span>
+                                                    <span class="chapter_release"><?php echo $chapter->getPublicationDate() ?></span>
+                                                </a></p>
+                                        </li>
+                                        <?php
+                                    }
+                                    ?>
+                                </ul>
+                            </div> <!-- Volume <?php echo $volume->getNumber(); ?>  "--> 
                             <?php
+                        }
+                        ?>
 
-                    } catch (Exception $e) {
-                        die('Error : ' . $e->getMessage());
-                    }
-                } else {
-                    header("Location:homePage.php");
+                    </div> <!-- chapters -->
+                    <?php
+                } catch (Exception $e) {
+                    die('Error : ' . $e->getMessage());
                 }
                 ?>
 
             </main>
-
         </div> <!-- page -->
     </body>
 </html>
