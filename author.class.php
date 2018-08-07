6897b440b2fe2661c9615bc5eeabe468436a2b46
hlebon <henri.lebon@res-group.com>
2018-08-07 09:34:23 +0200

    class author la génration de la liste de comics n'est faite que si la function getComics est appelée une première fois

--- author.class.php
+++ author.class.php
@@ -21,9 +21,10 @@
 		}
 
 		function getComics() {
-			if (!$ComicsLoaded) {
+			if (!$this->ComicsLoaded) {
 				//On ne crée la liste d'objet comic que si on en a besoin car l'objet Auther peut être utilisé sans qu'on ai besoin de ces comics
 				$this->SetComics();
+				$$this->ComicsLoaded = true;
 			}
         	foreach ($this->Comics as $comic) {
                 yield $comic;
