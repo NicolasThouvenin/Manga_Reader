70f8944b6784a33c526d8ee89aa787e6d6b9392f
hlebon <henri.lebon@res-group.com>
2018-08-07 23:54:24 +0200

    procédures tockée de création de volume : retourne aussi le numéro de volume maintenant

--- sgbdr/createVolume.sql
+++ sgbdr/createVolume.sql
@@ -1,10 +1,10 @@
-DELIMITER $$
-CREATE PROCEDURE createVolume(IN inTitle VARCHAR(255), IN inSynopsis VARCHAR(255), IN inStartDate VARCHAR(10), IN inComicId INTEGER, OUT lastVolumeId INTEGER)
-/* Cette procédure créer un nouveau tome dans la base et retourne son Id. Le numéro du tome est calculé automatiquement. */
+DELIMITER ;;
+CREATE PROCEDURE createVolume(IN inTitle VARCHAR(255), IN inSynopsis VARCHAR(255), IN inStartDate VARCHAR(10), IN inComicId INTEGER,
+					OUT lastVolumeId INTEGER, OUT lastVolumeNumber INTEGER)
+/* Cette procédure créer un nouveau tome dans la base et retourne son Id et son numéro. Le numéro du tome est calculé automatiquement. */
 BEGIN
-	DECLARE inNumber INTEGER;
-	SELECT Count(*) + 1 INTO inNumber FROM volumes WHERE comicId = inComicId;
+	SELECT Count(*) + 1 INTO lastVolumeNumber FROM volumes WHERE comicId = inComicId;
 	SET lastVolumeId = -1;
-	INSERT INTO volumes (Title, Number, Synopsis, StartDate, comicId) VALUES (inTitle, inNumber, inSynopsis, inStartDate, inComicId);
+	INSERT INTO volumes (Title, Number, Synopsis, StartDate, comicId) VALUES (inTitle, lastVolumeNumber, inSynopsis, inStartDate, inComicId);
 	SET lastVolumeId = LAST_INSERT_ID();
-END $$
+END ;;
