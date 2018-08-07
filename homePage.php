d0a53c2abb8649abff976422c3658c071ca608a0
Nico <thouvenin.nicolas@live.fr>
2018-08-07 17:38:31 +0200

    creation user profile

--- homePage.php
+++ homePage.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <title>Home</title>
         <link rel="stylesheet" type="text/css" href="css\main_lg.css">
+        <link rel="icon" href="ressources/favicon.ico" type="image/x-icon" />
     </head>
     <body>
         <div class="page">
@@ -18,8 +19,7 @@
                     if (isset($_COOKIE['authentified'])) {
                         $user = unserialize($_COOKIE['authentified']);
                         // if a user is connected
-                        //echo "<p class='userName'><a href='profile.php'>" . $user->getLogin() . "</a></p>";
-                        echo "<p class='userName'>" . $user->getLogin() . "</p>";
+                        echo "<p class='userName'><a href='profile.php'>" . $user->getLogin() . "</a></p>";
                         echo "<p><a class='userName' href='disconnect.php'>Log out</a></p>";
                     } else {
                         ?>
