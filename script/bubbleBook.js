aebf523fd1c707d26961657085920a9dddc58e95
Nico <thouvenin.nicolas@live.fr>
2018-08-08 10:11:45 +0200

    bubbleBook.js updated

--- script/bubbleBook.js
+++ script/bubbleBook.js
@@ -6,19 +6,24 @@ $(document).ready(function () {
      */
     genres = ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Historical", "Horror", "Sci-fi"];
     /**/
-     genreIds = $("#book_genre")[0].innerHTML;
-     listGenre = "";
-     for (i = 1; i < 9; i++) {
-     if (genreIds.search(i) !== -1) {
-     listGenre += genres[i] + ", ";
-     }
-     }
-     $("#book_genre")[0].innerHTML = listGenre.slice(0, -2) + ".";
-        
-     $("#book_author")[0].innerHTML = $("#book_author")[0].innerHTML.slice(0, -2);
-     
-    
-     // Put the title of the comic as the page title
+    genreIds = $("#book_genre")[0].innerHTML;
+    listGenre = "";
+    for (i = 1; i < 9; i++) {
+        if (genreIds.search(i) !== -1) {
+            listGenre += genres[i] + ", ";
+        }
+    }
+    $("#book_genre")[0].innerHTML = listGenre.slice(0, -2) + ".";
+
+    $("#book_author")[0].innerHTML = $("#book_author")[0].innerHTML.slice(0, -2);
+
+
+    // Put the title of the comic as the page title
     document.title = title;
 
 });
+
+function toggleForm() {
+    document.getElementById("new_volume_form").style.display = (document.getElementById("new_volume_form").style.display === "" ? "block" : "");
+
+}
