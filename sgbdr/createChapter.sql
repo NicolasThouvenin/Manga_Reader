a21664bfce93e17a2a3065fe613d2d5353124203
hlebon <henri.lebon@res-group.com>
2018-08-08 15:34:25 +0200

    la procédure createChapter retourne le number

--- sgbdr/createChapter.sql
+++ sgbdr/createChapter.sql
@@ -1,14 +1,13 @@
-DELIMITER $$
-CREATE PROCEDURE createChapter(IN inTitle VARCHAR(255), IN inSynopsis VARCHAR(255), IN inVolumeId INTEGER, OUT lastChapterId INTEGER)
+DELIMITER ;;
+CREATE PROCEDURE createChapter(IN inTitle VARCHAR(255), IN inSynopsis VARCHAR(255), IN inVolumeId INTEGER, OUT lastChapterId INTEGER, OUT lastChapterNumber INTEGER)
 /* Cette procédure créer un nouveau tome dans la base et retourne son Id. Le numéro du tome est calculé automatiquement. */
 BEGIN
-	DECLARE inNumber INTEGER;
-	SELECT Count(*) + 1 INTO inNumber FROM chapters
+	SELECT Count(*) + 1 INTO lastChapterNumber FROM chapters
 	JOIN volumes
 	ON volumeId = volumes.Id
 	JOIN comics
 	ON comics.Id = volumes.comicId;
 	SET lastChapterId = -1;
-	INSERT INTO chapters (Title, Number, Synopsis, volumeId) VALUES (inTitle, inNumber, inSynopsis, inVolumeId);
+	INSERT INTO chapters (Title, Number, Synopsis, volumeId) VALUES (inTitle, lastChapterNumber, inSynopsis, inVolumeId);
 	SET lastChapterId = LAST_INSERT_ID();
-END $$
+END ;;
