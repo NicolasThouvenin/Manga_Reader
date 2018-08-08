3ea94f3c9b579813ecb581696a36a5f8f8d39ebf
geeklyhub <41900739+geeklyhub@users.noreply.github.com>
2018-08-08 15:17:59 +0200

    mise à jour de homePage
    ajout d'un nav

--- homePage.php
+++ homePage.php
@@ -1,73 +1,79 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Home</title>
-        <link rel="stylesheet" type="text/css" href="css\main_lg.css">
-        <link rel="icon" href="ressources/favicon.ico" type="image/x-icon" >
-    </head>
-    <body>
-        <div class="page">
-            <header>
-                <input class="add" type='button' onclick="location.href = 'designerHome.php';" value="Add Creation" class="creationButton">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="css\main_lg.css">
+    <link rel="icon" href="ressources/favicon.ico" type="image/x-icon" >
+</head>
+<body>
+    <div class="page">
+        <header>
 
-                <div class="displayUser">
-                    <?php
-                    require('required.php');
-                    
-                    session_start();
-                    if (isset($_COOKIE['authentified'])) {
-                        $user = unserialize($_COOKIE['authentified']);
+
+      <input class="add" type='button' onclick="location.href = 'designerHome.php';" value="Add Creation" class="creationButton">
+
+      <div class="displayUser">
+        <?php
+        require('required.php');
+
+        session_start();
+        if (isset($_COOKIE['authentified'])) {
+            $user = unserialize($_COOKIE['authentified']);
                         // if a user is connected
-                        echo "<p class='userName'><a href='profile.php'>" . $user->getLogin() . "</a></p>";
-                        echo "<p><a class='userName' href='disconnect.php'>Log out</a></p>";
-                    } else {
-                        ?>
-                        <input type='button' onclick="location.href = 'login.php';" value="Log in" class="regularButton">
-                        <input type='button' onclick="location.href = 'register.php';" value="Register" class="registerButton">
-                        <?php
-                    }
-                    ?>
-                </div> <!-- displayUser -->
 
-                <div class="logo"><a href="homePage.php">LOGO</a></div>
+            echo "<p class='userName'><a href='profile.php'>" . $user->getLogin() . "</a></p>";
+            echo "<p><a class='userName' href='disconnect.php'>Log out</a></p>";
 
-            </header>
+        } else {
+            ?>
+            <input type='button' onclick="location.href = 'login.php';" value="Log in" class="regularButton">
+            <input type='button' onclick="location.href = 'register.php';" value="Register" class="registerButton">
+            <?php
+        }
+        ?>
+    </div> <!-- displayUser -->
 
-            <main>
-                <div class="banner">
-                    <input id="searchbar" type="search" name="q" placeholder="search comic or author">
-                </div> <!-- banner -->
-                <?php
-                try {
-                    include 'connection.php';
 
-                    echo "<p class='log'>Connection successful.<br>";
+<div class="logo"><a href="homePage.php"><img src="ressources/bubbleLogo.png"></a></div>
 
-                    $stmt = $db->prepare('CALL getComicsWithValidatedChapter()');
-                    $stmt->execute();
-                    if ($stmt->rowCount() == 0) {
-                        echo "<p class='error'>Sorry we haven't found any results matching this search.</p>";
-                    } else {
-                        while ($line = $stmt->fetch()) {
+</header>
+
+<main>
+    <div class="banner">
+        <input id="searchbar" type="search" name="q" placeholder="search comic or author">
+    </div> <!-- banner -->
+    <?php
+    try {
+        include 'connection.php';
+
+        echo "<p class='log'>Connection successful.<br>";
+
+        $stmt = $db->prepare('CALL getComicsWithValidatedChapter()');
+        $stmt->execute();
+        if ($stmt->rowCount() == 0) {
+            echo "<p class='error'>Sorry we haven't found any results matching this search.</p>";
+        } else {
+            while ($line = $stmt->fetch()) {
 
                             // Path to the comic cover
-                            $coverPath = "comics\\" . $line['Id'] . "\\cover" . "." . $line['CoverExt'];
-                            ?>
-
-                            <article id="comicId_<?php echo $line['Id'] ?>">
-                                <a href="comic.php?bookId=<?php echo $line['Id'] ?>"><img src="<?php echo $coverPath ?>" alt="cover" width="150" height="225"></a>
-                                <p class="comics_name"><a href="comic.php?bookId=<?php echo $line['Id'] ?>"><?php echo $line['Title'] ?></a><p>
-                            </article>
-
-                            <?php
-                        }
-                    }
-                } catch (Exception $e) {
-                    die('Error : ' . $e->getMessage());
-                }
+                $coverPath = "comics\\" . $line['Id'] . "\\cover" . "." . $line['CoverExt'];
                 ?>
-            </main>
-        </div> <!-- page -->
-    </body>
+
+                <article id="comicId_<?php echo $line['Id'] ?>">
+                    <a href="comic.php?bookId=<?php echo $line['Id'] ?>"><img src="<?php echo $coverPath ?>" alt="cover" width="150" height="225"></a>
+                    <p class="comics_name"><a href="comic.php?bookId=<?php echo $line['Id'] ?>"><?php echo $line['Title'] ?></a><p>
+                    </article>
+
+                    <?php
+                }
+            }
+        } catch (Exception $e) {
+            die('Error : ' . $e->getMessage());
+        }
+        ?>
+    </main>
+</div> <!-- page -->
+</body>
 </html>
