a4651745686d9b9707931e4c21b7a68fb12c1075
geeklyhub <41900739+geeklyhub@users.noreply.github.com>
2018-08-10 10:29:53 +0200

    update disconnect.php
    added comment

--- disconnect.php
+++ disconnect.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-$_SESSION = array();
-session_destroy();
-setcookie('authentified', "", time()-3600);
-header("Location:homePage.php");
+$_SESSION = array(); //removing session's variables
+//session_unset(); // removing session's variables //obsolete
+setcookie('authentified', "", time()-3600); // negative time do not preserve cookies after closing 
+session_destroy(); // unseting the session
+header("Location:homePage.php"); // redirect to homePage.php
 ?>
