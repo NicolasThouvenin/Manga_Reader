dce9f7ce6041c517e1387c56d35145a1c8efa2e7
hlebon <henri.lebon@res-group.com>
2018-08-10 13:06:58 +0200

    réparation de newComic.php

--- newComic.php
+++ newComic.php
@@ -3,96 +3,85 @@
     <head>
         <meta charset="utf-8">
         <title>New Comic</title>
-        <link rel="stylesheet" type="text/css" href="css\main_lg.css">
+        <link rel="stylesheet" type="text/css" href="css/main_lg.css">
         <link rel="icon" href="ressources/favicon.ico" type="image/x-icon" >
     </head>
     <body>
         <div class="page">
             <header>
                 <input class="add" type='button' onclick="location.href = 'designerHome.php';" value="Add Creation" style="visibility: hidden;">
-
                 <div class="displayUser">
                     <?php
-                    require('required.php');
-
-                    session_start();
-                    if (isset($_COOKIE['authentified'])) {
-                        $user = unserialize($_COOKIE['authentified']);
-                        // if a user is connected
-                        echo "<p class='userName'>" . $user->getLogin() . "</p>";
-                        echo "<p><a class='userName' href='disconnect.php'>Log out</a></p>";
-                    } else {
-                        header("Location:homePage.php");
-                    }
+                        require('required.php');
+                        session_start();
+                        if (isset($_COOKIE['authentified'])) {
+                            $user = unserialize($_COOKIE['authentified']);
+                            // if a user is connected and authentified
+                            echo "<p class='userName'>".$user->getLogin()."</p>";
+                            echo "<p><a class='userName' href='disconnect.php'>Log out</a></p>";
+                        } else {
+                            header("Location:homePage.php");
+                        }
                     ?>
                 </div> <!-- displayUser -->
-
                 <div class="logo"><a href="homePage.php">LOGO</a></div>
-
             </header>
-
             <main>
                 <?php
-                if (isset($_POST['submit'])) { // if the form is validated, it will begin to check the files to upload
-                    try {
-                        $check = explode("/", mime_content_type($_FILES["cover"]["tmp_name"])); // checking content type
-                        if ($check[0] !== "image") {
-                            throw new Exception("\nFile is not an image : " . $e->getMessage());
+                    if (isset($_POST['submit'])) { // if the form is validated, it will begin to check the files to upload
+                        try {
+                            $check = explode("/", mime_content_type($_FILES["cover"]["tmp_name"])); // checking content type
+                            if ($check[0] !== "image") {
+                                throw new Exception("\nFile is not an image : " . $e->getMessage());
+                            }
+                            require('connection.php');
+                            $comicTitle = htmlentities($_POST['comicTitle']);
+                            $comicSynopsis = htmlentities($_POST['synopsis']);
+                            $date = htmlentities($_POST['inStartDate']);
+                            $userId = $user->getId();
+                            $createComic = $db->prepare("CALL createComic(:Title, :Synopsis, :StartDate, :AuthorId, :inCoverExt, @lastComicId)");
+                            $createComic->bindParam(':Title', $comicTitle, PDO::PARAM_STR, 255);
+                            $createComic->bindParam(':Synopsis', $comicSynopsis, PDO::PARAM_STR, 255);
+                            $createComic->bindParam(':StartDate',$date , PDO::PARAM_STR, 10);
+                            $createComic->bindParam(':AuthorId', $userId, PDO::PARAM_INT);
+                            $createComic->bindParam(':inCoverExt', $check[1], PDO::PARAM_STR, 10);
+                            $createComic->execute();
+                            $createComic->closeCursor();
+
+                            $result = $db->query("SELECT @lastComicId")->fetch(PDO::FETCH_ASSOC);
+                            $comic = new Comic($result['@lastComicId']);
+                            $comic->AddCover($_FILES["cover"]["tmp_name"]);
+                            $comic->createVolume(htmlentities($_POST['volumeTitle']), htmlentities($_POST['volumeSynopsis']));
+                            header("Location:newChapter.php?volumeId=".$comic->getLastVolumeId());
+
+                        } catch (Exception $e) {
+                            die('Error during Comic creation : '.$e->getMessage());
                         }
-                        require('connection.php');
-                        $comicTitle = htmlentities($_POST['comicTitle']);
-                        $comicSynopsis = htmlentities($_POST['synopsis']);
-                        $date = htmlentities($_POST['inStartDate']);
-                        $userId = $user->getId();
-                        $createComic = $db->prepare("CALL createComic(:Title, :Synopsis, :StartDate, :AuthorId, :inCoverExt, @lastComicId)");
-                        $createComic->bindParam(':Title', $comicTitle, PDO::PARAM_STR, 255);
-                        $createComic->bindParam(':Synopsis', $comicSynopsis, PDO::PARAM_STR, 255);
-                        $createComic->bindParam(':StartDate',$date , PDO::PARAM_STR, 10);
-                        $createComic->bindParam(':AuthorId', $userId, PDO::PARAM_INT);
-                        $createComic->bindParam(':inCoverExt', $check[1], PDO::PARAM_STR, 10);
-                        $createComic->execute();
-                        $createComic->closeCursor();
-
-
-                        $result = $db->query("SELECT @lastComicId")->fetch(PDO::FETCH_ASSOC);
-                        $comic = new Comic($result['@lastComicId']);
-                        $comic->AddCover($_FILES["cover"]["tmp_name"]);
-                        $comic->createVolume(htmlentities($_POST['volumeTitle']), htmlentities($_POST['volumeSynopsis']));
-                        $volumeId = $comic->getLastVolumeId();
-                        header("Location:newChapter.php?volumeId=".$volumeId);
-                    } catch (Exception $e) {
-                        die('Error during Comic creation : ' . $e->getMessage());
+                    } else {
+                        echo '<div id="new_comic"> // <!--creating a bloc for the new book\'s form-->
+                            <form method="POST" action="newComic.php" enctype="multipart/form-data"> 
+                                <p>Title : <input type="text" name="comicTitle" placeholder="New Comic Title" required></p>
+                                <p>Creation Date : <input type="date" name="inStartDate" value="<?php echo date("Y-m-d"); ?>" required></p> <!-- input for choose a date -->
+                                <p>Comic Synopsis : <textarea name="synopsis" placeholder="Comic Synopsis" cols="40" rows="3" required></textarea></p> <!-- input for comments area -->
+                                <label>Genres : <select id="genres" multiple name="genres[]" required="required"> <!-- input for selecting in a list of genres -->
+                                        <option value="action">Action</option> 
+                                        <option value="adventure">Adventure</option>
+                                        <option value="comedy">Comedy</option>
+                                        <option value="drama">Drama</option>
+                                        <option value="fantasy">Fantasy</option>
+                                        <option value="historical">Historical</option>
+                                        <option value="horror">Horror</option>
+                                        <option value="scienceFiction">Sci-fi</option>
+                                    </select></label>
+                                <p>Comic cover picture : <input type="file" name="cover" id="cover" required></p> <!-- input for uploading files -->
+                                <p id="first_volume">First Volume Title : <input type="text" name="volumeTitle" placeholder="Volume Title" required></p>
+                                <p>Volume Synopsis : <textarea name="volumeSynopsis" placeholder="Volume Synopsis" cols="40" rows="3" required></textarea></p>
+                                <input type="submit" name="submit" value="Create New Comic">
+                            </form>
+                        </div>';
                     }
-                } else {
-                    ?>
-                    <div id="new_comic"> // <!--creating a bloc for the new book's form-->
-                        <form method="POST" action="newComic.php" enctype="multipart/form-data"> 
-                            <p>Title : <input type="text" name="comicTitle" placeholder="New Comic Title" required></p>
-                            <p>Creation Date : <input type="date" name="inStartDate" value="<?php echo date("Y-m-d"); ?>" required></p> <!-- input for choose a date -->
-                            <p>Comic Synopsis : <textarea name="synopsis" placeholder="Comic Synopsis" cols="40" rows="3" required></textarea></p> <!-- input for comments area -->
-                            <label>Genres : <select id="genres" multiple name="genres[]" required="required"> <!-- input for selecting in a list of genres -->
-                                    <option value="action">Action</option> 
-                                    <option value="adventure">Adventure</option>
-                                    <option value="comedy">Comedy</option>
-                                    <option value="drama">Drama</option>
-                                    <option value="fantasy">Fantasy</option>
-                                    <option value="historical">Historical</option>
-                                    <option value="horror">Horror</option>
-                                    <option value="scienceFiction">Sci-fi</option>
-                                </select></label>
-                            <p>Comic cover picture : <input type="file" name="cover" id="cover" required></p> <!-- input for uploading files -->
-                            <p id="first_volume">First Volume Title : <input type="text" name="volumeTitle" placeholder="Volume Title" required></p>
-                            <p>Volume Synopsis : <textarea name="volumeSynopsis" placeholder="Volume Synopsis" cols="40" rows="3" required></textarea></p>
-                            <input type="submit" name="submit" value="Create New Comic">
-                        </form>
-
-                    </div>
-                    <?php
                 ?>
-
-            </main> 
-
-
+            </main>
         </div> <!-- page -->
     </body>
 </html>
