8ea810cfffc9d0d61f7935b558787934537ad4d2
geeklyhub <41900739+geeklyhub@users.noreply.github.com>
2018-08-10 12:23:58 +0200

    update connection.php
    added comments

--- connection.php
+++ connection.php
@@ -1,4 +1,8 @@
 <?php
+    
+//     connection to Old database hosted by afip's local network
     // $db= New PDO('mysql:host=172.17.220.139;dbname=bubbleupdb','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+// connection to new database owned by PO
     $db= New PDO('mysql:host=ns3272345.ip-5-39-84.eu;dbname=bubbleupdb','chenille','DBiPrtYCI4qnTK3Q', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 ?>
