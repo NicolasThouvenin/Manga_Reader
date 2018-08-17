1dc789a0d660c1805e777b9bac74f6829b02e446
geeklyhub <41900739+geeklyhub@users.noreply.github.com>
2018-08-17 17:08:35 +0200

    Update emailValidation.php
    Déplacement et débuggage d'erreurs syntaxiques.
    Le PHPMailer a été déplacé dans AddUser pour la méthode POST de l'email. Cela n'altère rien.

--- emailValidation.php
+++ emailValidation.php
@@ -1,12 +1,7 @@
-
 <?php
 if (isset($_GET['userid']) && isset($_GET['emailkey']) && isset($_GET['sendemail'])) {
 	if ($_GET['sendemail'] == true) {
-		$subject = "Inscription confirmation to bubbleUp";
-		$message = "Hello,\nThanks you for your inscription to bubbleUp. Please, click to follow link to confirm your inscription :\n
-		http://localhost/projetWeb1/emailValidation.php?userid=".$_GET['userid'].'&emailkey='.$_GET['@emailkey'].'&sendemail=false'."\nRegards";
-		mail($checkedData['email'], $subject, $message);
-		$headerMessage = "You have received an email from bubbleUp to confirm your inscription. If you haven't received it check your unwanted mails";
+		$headerMessage = "You have received an email from bubbleUp to confirm your registration. If you haven't received it check your unwanted mails";
 		$resendEmailDisplay = 'initial';
 	} else {
 		$headerMessage = "Thanks you for your inscription to bubbleup";
@@ -18,47 +13,9 @@ if (isset($_GET['userid']) && isset($_GET['emailkey']) && isset($_GET['sendemail
 }
 ?>
 
-
-
-
-<?php
-	//Getting user mail:
-$userEmail = $_GET['email'];
-	//initiating PHPMailer
-require 'PHPMailer-5.2/PHPMailerAutoload.php';
-$mail= new PHPMailer;
-$mail->isSMTP();
-$mail->Host="smtp.gmail.com";
-$mail->Port=465;
-$mail->SMTPAuth=true;
-$mail->SMTPSecure="ssl";
-
-	//webmail server credentials
-$mail->Username='blaisiustristodontor@gmail.com';
-$mail->Password='Hn7t31PxGvTy8';
-
-	//email sending setup
-$mail->setFrom('no-reply@bubbleup.fr','BubbleUp.fr');
-$mail->addAddress($userEmail);
-$mail->addReplyTo('pharob@superuser.fr');
-
-$mail->isHTML(true);
-$mail->Subject='Your BubbleUp account confirmation :';
-$mail->Body="Hello,\n
-Thank you for your registration to BubbleUp. Please, click on the link to activate your account :\n
-www.activersoncompte.etc...";
-if(!$mail->send()){
-	echo "Message couldn't be sent!";
-}
-else{
-	echo "You have received an email from BubbleUp, to confirm your registration. If you haven't received it check your unwanted emails";
-}
-
-?>
-
 <div>
 	<p><h2><?php echo $headerMessage ?></h2></p>
-	<input type="button" onclick="location.href =<?php echo 'emailValidation.php?userid='.$_GET['userid'].'&emailkey='.$_GET['@emailkey'].'&sendemail=true';?>" value="Resend email" style="display=<?php $resendEmailDisplay ; ?>">
+	<input type="button" onclick="location.href =<?php echo 'emailValidation.php?'.'&userid='.$_GET['userid'].'&emailkey='.$_GET['@emailkey'].'&sendemail=true';?>" value="Resend email" style="display=<?php $resendEmailDisplay ; ?>">
 </div>
 
 
