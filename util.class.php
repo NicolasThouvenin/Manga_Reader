5e419dd9bc3f468517b4abd3819a21eb34002d90
hlebon <henri.lebon@res-group.com>
2018-08-20 08:20:34 +0200

    la méthode getGenreOptions() avait été supprimée de la classe Util pour une raison inconnue

--- util.class.php
+++ util.class.php
@@ -8,5 +8,31 @@
             }
             return $data;
 		}
+
+
+		public static function getGenreOptions() {
+
+            /* This method returns the genre array */
+            
+            try {
+
+                require('connection.php');
+
+                $result = $db->prepare("SELECT * FROM genres");
+                $result->execute();
+
+                $options = '';
+
+                while ($line = $result->fetch()) {
+                    $options = $options.'<option value="'.$line['Id'].'">'.$line['Label'].'</option>';
+                }
+
+                return $options;
+
+            } catch (Exception $e) {
+                throw new Exception("<br>Erreur lors de la création de la liste d'options de genres : ". $e->getMessage());
+            }
+		}
+
 	}
 ?>
