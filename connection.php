11bec3266234b1fe2c489370223c092568d413a2
NicolasThouvenin <33023231+NicolasThouvenin@users.noreply.github.com>
2021-05-15 23:24:51 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,5 +4,5 @@
     // $db= New PDO('mysql:host=172.17.220.139;dbname=bubbleupdb','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 // connection to new database owned by PO
-    $db= New PDO('mysql:host=ns3272345.ip-5-39-84.eu;dbname=bubbleupdb','chenille','DBiPrtYCI4qnTK3Q', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $db= New PDO('mysql:host=******;dbname=*******','******','**********', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 ?>
